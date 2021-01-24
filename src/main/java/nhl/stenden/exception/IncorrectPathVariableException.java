9d18b09a3836e1ddd2cfedf633f6d4e6b743ca73
justin <games.j@live.nl>
2021-01-24 23:29:35 +0100

    added javadocs and hololive integration test
    added javadocs and hololive integration test

--- src/main/java/nhl/stenden/exception/IncorrectPathVariableException.java
+++ src/main/java/nhl/stenden/exception/IncorrectPathVariableException.java
@@ -1,5 +1,8 @@
 package nhl.stenden.exception;
 
+/**
+ * This class provides a custom exception for when an incorrect pathvariable is used
+ */
 public class IncorrectPathVariableException extends RuntimeException {
 
     public IncorrectPathVariableException(String message){
--- src/main/java/nhl/stenden/exception/IncorrectPropertiesException.java
+++ src/main/java/nhl/stenden/exception/IncorrectPropertiesException.java
@@ -1,5 +1,8 @@
 package nhl.stenden.exception;
 
+/**
+ * This class provides a custom exception for when an incorrect properties are used
+ */
 public class IncorrectPropertiesException extends RuntimeException {
 
     public IncorrectPropertiesException(String message){
